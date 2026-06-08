693ae48694c4a48fd963ed7fbbceeac1f71dd97e
Vladimir Perminov <busygose@gmail.com>
2026-06-08 12:32:30 +0300

    setup: replace myWelcome with myWelcomeSetup

--- welcome-module.nix
+++ welcome-module.nix
@@ -22,18 +22,12 @@
         environment.systemPackages = [
             (pkgs.writeShellScriptBin "welcome-custom" "echo '${config.services.myWelcome.text}'")
         ];
-        systemd.services.myWelcome = {
-            description = "Write welcome text to /etc/welcome.txt";
-            wantedBy = [ "multi-user.target" ];
-            serviceConfig = {
-                Type = "oneshot";
-                ExecStart = "${pkgs.writeShellScript "welcome-custom" ''
-                touch /etc/welcome.txt
-                echo "${config.services.myWelcome.text}" >> /etc/welcome.txt #Yes, I want to store all of
-                #them
-                ''}";
-            };
+        system.activationScripts.myWelcomeSetup = {
+            text = ''
+                mkdir -p /etc/welcome-dir
+                echo '${config.services.myWelcome.text}' > /etc/welcome-dir/message.txt
+            '';
+            deps = [ "etc" ];
         };
     };
-
 }
