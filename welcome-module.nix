089de9a03d31d7c15c7b6243b1a0f1d2b8d5a07b
Vladimir Perminov <busygose@gmail.com>
2026-06-08 11:31:44 +0300

    services: add myService

--- welcome-module.nix
+++ welcome-module.nix
@@ -22,6 +22,18 @@
         environment.systemPackages = [
             (pkgs.writeShellScriptBin "welcome-custom" "echo '${config.services.myWelcome.text}'")
         ];
+        systemd.services.myWelcome = {
+            description = "Write welcome text to /etc/welcome.txt";
+            wantedBy = [ "multi-user.target" ];
+            serviceConfig = {
+                Type = "oneshot";
+                ExecStart = "${pkgs.writeShellScript "welcome-custom" ''
+                touch /etc/welcome.txt
+                echo "${config.services.myWelcome.text}" >> /etc/welcome.txt #Yes, I want to store all of
+                #them
+                ''}";
+            };
+        };
     };
 
 }
