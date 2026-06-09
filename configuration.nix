dda58c25de7e33bee30c56585844d15a36ab5639
Vladimir Perminov <busygose@gmail.com>
2026-06-09 09:54:54 +0300

    format: format configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -6,11 +6,11 @@
 
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [
       ./hardware-configuration.nix
     ];
 
-  # Use the systemd-boot EFI boot loader.
+  # BOOT
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.systemd-boot.windows = {
@@ -20,10 +20,61 @@
     };
   };
 
-  networking.hostName = "nixos-x390"; # Define your hostname.
-
-  # Configure network connections interactively with nmcli or nmtui.
+  # NETWORK
+  networking.hostName = "nixos-x390";
   networking.networkmanager.enable = true;
+
+  # Font packages
+  fonts = {
+    packages = with pkgs; [
+      inter
+      nerd-fonts.jetbrains-mono
+      liberation_ttf
+      noto-fonts-color-emoji
+      noto-fonts
+      corefonts
+    ];
+  };
+
+  # X11
+  services.xserver.enable = true;
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
+  services.xserver.xkb = {
+    layout = "us,ru";
+    options = "grp:alt_shift_toggle";
+  };
+
+  # USERS
+  users.users.fkngoose = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" "video" ];
+    packages = with pkgs; [ ];
+    homeMode = "700";
+    initialPassword = "1234"; # Don't forget to set a password with ‘passwd’
+  };
+   
+  # PROGRAMS
+  programs.firefox.enable = true;
+  programs.git.enable = true;
+  programs.throne.enable = true;
+  programs.throne.tunMode.enable = true;
+
+  # SERVICES
+  services.libinput.enable = true;
+  services.pipewire = {
+    enable = true;
+    pulse.enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+  };
+  services.printing.enable = true;
+
+  # MISC
+  nixpkgs.config.allowUnfree = true; # For propietary drivers
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  security.rtkit.enable = true;
+
 
   # Set your time zone.
   # time.timeZone = "Europe/Amsterdam";
@@ -39,70 +90,6 @@
   #   keyMap = "us";
   #   useXkbConfig = true; # use xkb.options in tty.
   # };
-
-
-  # Font settings
-  fonts = {
-    packages = with pkgs; [
-      inter
-      nerd-fonts.jetbrains-mono
-      liberation_ttf
-      noto-fonts-color-emoji
-      noto-fonts
-      corefonts
-    ];
-  };
-
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-  services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
-  services.xserver.xkb = {
-    layout = "us,ru";
-    options = "grp:alt_shift_toggle";
-  };
-
-  nixpkgs.config.allowUnfree = true; #For propietary drivers
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
-  # Configure keymap in X11
-  # services.xserver.xkb.layout = "us";
-  # services.xserver.xkb.options = "eurosign:e,caps:escape";
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable sound.
-  # services.pulseaudio.enable = true;
-  # OR
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    pulse.enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-  };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  services.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.fkngoose = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" "networkmanager" "video" ]; # Enable ‘sudo’ for the user.
-    packages = with pkgs; [
-      tree
-    ];
-    homeMode = "700";
-    initialPassword = "1234";
-  };
-   
-
-  # PROGRAMS
-   programs.firefox.enable = true;
-   programs.git.enable = true;
-   programs.throne.enable = true;
-   programs.throne.tunMode.enable = true;
 
   # List packages installed in system profile.
   # You can use https://search.nixos.org/ to find more packages (and options).
